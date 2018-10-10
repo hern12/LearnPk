d16dc693bbb1038972dd3ec090477b3abdf38efa
Hern12 <hern.suwannasam@gmail.com>
2018-10-10 17:54:33 +0700

    update project and then go home

---
+++ src/firebase.ts
@@ -0,0 +1,10 @@
+  import firebase from 'firebase'
+  var config = {
+    apiKey: "AIzaSyAC3KbAQk5NyaPgdXfjyq5A8kGrAZTl51U",
+    authDomain: "ohmyweb-fa98b.firebaseapp.com",
+    databaseURL: "https://ohmyweb-fa98b.firebaseio.com",
+    projectId: "ohmyweb-fa98b",
+    storageBucket: "ohmyweb-fa98b.appspot.com",
+    messagingSenderId: "638405276478"
+  };
+  firebase.initializeApp(config);
